e53aa557d1856e1eae3dc967741091e2aa0566a4
junjuny0227 <junjuny0227@gmail.com>
2026-09-07 19:31:40 +0900

    update(app): React Query 기본 옵션 설정

--- apps/admin/src/app/providers.tsx
+++ apps/admin/src/app/providers.tsx
@@ -6,7 +6,17 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export const Providers = ({ children }: Readonly<{ children: React.ReactNode }>) => {
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+            retry: 1,
+          },
+        },
+      }),
+  );
 
   return (
     <QueryClientProvider client={queryClient}>
