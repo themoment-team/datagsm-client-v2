53006d5daeb8c835eb0003c41e8b55aa0a654dc7
junjuny0227 <junjuny0227@gmail.com>
2026-09-08 01:13:40 +0900

    fix(api): Axios 최신 버전 타입 호환

--- packages/core/src/shared/api/methods.ts
+++ packages/core/src/shared/api/methods.ts
@@ -1,16 +1,16 @@
 import { axiosInstance } from './client';
 
-export const del = async <T>(...args: Parameters<typeof axiosInstance.delete>) =>
-  await axiosInstance.delete<T, T>(...args);
+export const del = async <T>(...args: Parameters<typeof axiosInstance.delete>): Promise<T> =>
+  (await axiosInstance.delete<T, T>(...args)) as T;
 
-export const get = async <T>(...args: Parameters<typeof axiosInstance.get>) =>
-  await axiosInstance.get<T, T>(...args);
+export const get = async <T>(...args: Parameters<typeof axiosInstance.get>): Promise<T> =>
+  (await axiosInstance.get<T, T>(...args)) as T;
 
-export const patch = async <T>(...args: Parameters<typeof axiosInstance.patch>) =>
-  await axiosInstance.patch<T, T>(...args);
+export const patch = async <T>(...args: Parameters<typeof axiosInstance.patch>): Promise<T> =>
+  (await axiosInstance.patch<T, T>(...args)) as T;
 
-export const post = async <T>(...args: Parameters<typeof axiosInstance.post>) =>
-  await axiosInstance.post<T, T>(...args);
+export const post = async <T>(...args: Parameters<typeof axiosInstance.post>): Promise<T> =>
+  (await axiosInstance.post<T, T>(...args)) as T;
 
-export const put = async <T>(...args: Parameters<typeof axiosInstance.put>) =>
-  await axiosInstance.put<T, T>(...args);
+export const put = async <T>(...args: Parameters<typeof axiosInstance.put>): Promise<T> =>
+  (await axiosInstance.put<T, T>(...args)) as T;
--- packages/core/src/shared/api/server-methods.ts
+++ packages/core/src/shared/api/server-methods.ts
@@ -1,16 +1,16 @@
 import { serverAxiosInstance } from './server';
 
-export const del = async <T>(...args: Parameters<typeof serverAxiosInstance.delete>) =>
-  await serverAxiosInstance.delete<T, T>(...args);
+export const del = async <T>(...args: Parameters<typeof serverAxiosInstance.delete>): Promise<T> =>
+  (await serverAxiosInstance.delete<T, T>(...args)) as T;
 
-export const get = async <T>(...args: Parameters<typeof serverAxiosInstance.get>) =>
-  await serverAxiosInstance.get<T, T>(...args);
+export const get = async <T>(...args: Parameters<typeof serverAxiosInstance.get>): Promise<T> =>
+  (await serverAxiosInstance.get<T, T>(...args)) as T;
 
-export const patch = async <T>(...args: Parameters<typeof serverAxiosInstance.patch>) =>
-  await serverAxiosInstance.patch<T, T>(...args);
+export const patch = async <T>(...args: Parameters<typeof serverAxiosInstance.patch>): Promise<T> =>
+  (await serverAxiosInstance.patch<T, T>(...args)) as T;
 
-export const post = async <T>(...args: Parameters<typeof serverAxiosInstance.post>) =>
-  await serverAxiosInstance.post<T, T>(...args);
+export const post = async <T>(...args: Parameters<typeof serverAxiosInstance.post>): Promise<T> =>
+  (await serverAxiosInstance.post<T, T>(...args)) as T;
 
-export const put = async <T>(...args: Parameters<typeof serverAxiosInstance.put>) =>
-  await serverAxiosInstance.put<T, T>(...args);
+export const put = async <T>(...args: Parameters<typeof serverAxiosInstance.put>): Promise<T> =>
+  (await serverAxiosInstance.put<T, T>(...args)) as T;
