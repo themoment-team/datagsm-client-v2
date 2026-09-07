a432dedd37f66b739fc1e3e626f9c9dc48ecebbf
junjuny0227 <junjuny0227@gmail.com>
2026-09-07 15:35:39 +0900

    update(ui): UI 배럴 export 추가

--- apps/docs/src/app/page.tsx
+++ apps/docs/src/app/page.tsx
@@ -1,7 +1,5 @@
 import Image from 'next/image';
-import { Card } from '@repo/ui/card';
-import { Gradient } from '@repo/ui/gradient';
-import { TurborepoLogo } from '@repo/ui/turborepo-logo';
+import { Card, Gradient, TurborepoLogo } from '@repo/ui';
 
 const LINKS = [
   {
